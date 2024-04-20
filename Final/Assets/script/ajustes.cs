952b3f69990b551a28dc46aad579128b8141d962
A01751213 <117104365+A01751213@users.noreply.github.com>
2024-04-19 20:04:25 -0600

    Squashed commit of the following:
    commit ba2e8d90a6dfe352d95ccc31441290f23415458f
    Author: A01749898 <A01749898@tec.mx>
    Date:   Fri Apr 19 19:54:06 2024 -0600
    
        Update ajustes.cs
    
    commit 2f559b76c6fdb12892be5508dd1f696ca07d5bb3
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 20:41:52 2024 -0600
    
        inicio de ajustes
    
    commit ecc2ce45ee2e7c029d7ff24d62d1951d8d6624e6
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 19:40:36 2024 -0600
    
        HUD_contadores
    
    commit 4e996e04503392e04c8f7007765896a22ff6648c
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 16:11:24 2024 -0600
    
        Boton de creadores
    
    commit 2f9620daa83b759775174a81a74bc50b8e18f7a8
    Merge: 5c5de25 195a2b4
    Author: Ignacio <117104365+A01751213@users.noreply.github.com>
    Date:   Tue Apr 16 00:45:46 2024 -0600
    
        pt2
    
    commit 5c5de257e617ac56860e0c6b7de15ad4564f31b7
    Author: Ignacio <117104365+A01751213@users.noreply.github.com>
    Date:   Tue Apr 16 00:44:08 2024 -0600
    
        hud pt2
    
    commit 195a2b44879834429e8877b684740513aa7c19cd
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:42:28 2024 -0600
    
        prueba7
    
    commit a1774a95615e308a601f5baa5f7f90ac56403e84
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:42:14 2024 -0600
    
        prueba6
    
    commit 990a5a6a5bc3efd0008daac8f94c92995264a829
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:41:57 2024 -0600
    
        prueba5
    
    commit 5f6a4c1a9ca0b68f5a3e02f2a8b9d79e91cf38ce
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:41:01 2024 -0600
    
        prueba5
    
    commit 60a504c529754de14d9fbc5cc251570491bd359e
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:39:46 2024 -0600
    
        prueba4
    
    commit 532dc30f2be3a9f27b33ede7b24815d6de3f6dab
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:39:18 2024 -0600
    
        prueba3
    
    commit cf7eb95881de8c73d6ea5c3c696cf54c5b8d2a1c
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:38:20 2024 -0600
    
        prueba2
    
    commit 833528fa8a4d644fdbadd8f693dac21ddeedd7fb
    Author: A01749898 <A01749898@tec.mx>
    Date:   Tue Apr 16 00:36:16 2024 -0600
    
        menu_prueba

---
+++ Final/Assets/script/ajustes.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ajustes : MonoBehaviour
+{
+    private bool pulsado = false;
+
+    private bool salir = false;
+    public GameObject pantallaAjustes;
+    public GameObject HUD;
+
+    public void Ajustes(){
+        //Activa y desactiva la pantalla de creadores al pulsar el boton
+        if(pulsado){
+            pantallaAjustes.SetActive(false);
+            HUD.SetActive(true);
+            pulsado = false;
+        }else{
+            print("prendiendo ajustes");
+            pantallaAjustes.SetActive(true);
+            HUD.SetActive(false);
+            pulsado = true;
+        }
+    }
+
+    /*
+    public void Cerrar(){
+        if(salir){
+            pantallaAjustes.SetActive(true);
+            HUD.SetActive(false);
+            salir = false;
+        }else{
+            pantallaAjustes.SetActive(false);
+            HUD.SetActive(true);
+            salir = true;
+        }
+    }*/
+}
