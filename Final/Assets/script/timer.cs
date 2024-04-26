7b3bba0ac8e038952eb7604131e3a9598999977c
Ignacio <117104365+A01751213@users.noreply.github.com>
2024-04-26 05:32:00 -0600

    Timer al 80%

---
+++ Final/Assets/script/timer.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+public class Timer : MonoBehaviour{
+
+    [SerializeField] private TMP_Text timertext;
+    [SerializeField, Tooltip("Tiempo en segundos") ] private float timerTime;
+
+    private int minutes, seconds, cents;
+    private void Update()
+    {
+
+        timerTime -= Time.deltaTime;
+
+        if (timerTime <0) timerTime = 0;
+
+        minutes = (int)(timerTime / 60f);
+        seconds = (int)(timerTime - minutes * 60f);
+        cents = (int)((timerTime-(int)timerTime) *100f);
+
+        timertext.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+    }
+}
